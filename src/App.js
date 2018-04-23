e5804f96ef28e054aba063a318674f7fef72bbf7
arshel <arshelmelfor@hotmail.com>
2018-04-23 17:58:34 +0200

    git init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <header>
+              <nav>
+                  <ul className="ul">
+                      <li>1</li>
+                      <li>2</li>
+                      <li>3</li>
+                      <li>4</li>
+                  </ul>
+              </nav>
+          </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
